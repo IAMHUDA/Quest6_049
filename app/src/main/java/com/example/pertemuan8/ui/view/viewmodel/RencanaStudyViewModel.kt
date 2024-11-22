b283f51fceef494571f5bebb493459742c16a43e
IAMHUDA <hudamiftahul362@gmail.com>
2024-11-22 15:26:43 +0700

    menambahkan function setmatakuliah

--- app/src/main/java/com/example/pertemuan8/ui/view/viewmodel/RencanaStudyViewModel.kt
+++ app/src/main/java/com/example/pertemuan8/ui/view/viewmodel/RencanaStudyViewModel.kt
@@ -1,4 +1,16 @@
 package com.example.pertemuan8.ui.view.viewmodel
 
-class RencanaStudyViewModel {
+import androidx.lifecycle.ViewModel
+import com.example.pertemuan8.model.RencanaStudy
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+
+class RencanaStudyViewModel : ViewModel{
+    private val _krsState = MutableStateFlow(RencanaStudy())
+    val krsStateUi: StateFlow<RencanaStudy> = _krsState.asStateFlow()
+
+    fun setMataKuliah(){
+
+    }
 }
