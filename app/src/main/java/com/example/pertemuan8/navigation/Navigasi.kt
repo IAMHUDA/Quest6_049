6e52fabc9be0924aa35fb3cf6b42f8e99fcc20e3
IAMHUDA <hudamiftahul362@gmail.com>
2024-11-22 16:03:53 +0700

    menambahkan route halaman peminatan

--- app/src/main/java/com/example/pertemuan8/navigation/Navigasi.kt
+++ app/src/main/java/com/example/pertemuan8/navigation/Navigasi.kt
@@ -59,5 +59,18 @@ fun MahasiswaApp(
                 onbackbuttonClicked = {navController.popBackStack()}
             )
         }
+
+        composable(route = Halaman.Peminatan.name) {
+            RencanaStudyView(
+                mahasiswa = mahasiswaUiState,
+                onSubmitButton = {
+                    RencanaStudyViewModel.saveDataKRS(it)
+                    navController.navigate(Halaman.TampilKrs.name)
+                },
+                onbackbuttonClicked = {
+                    navController.popBackStack()
+                }
+            )
+        }
     }
 }
