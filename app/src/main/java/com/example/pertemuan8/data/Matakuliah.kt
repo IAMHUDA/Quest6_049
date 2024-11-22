d70695ac0349cbc555fa0b344dd2bd3a33dd2a66
IAMHUDA <hudamiftahul362@gmail.com>
2024-11-22 14:20:22 +0700

    menambahkan object matakuliah

---
+++ app/src/main/java/com/example/pertemuan8/data/Matakuliah.kt
@@ -0,0 +1,10 @@
+package com.example.pertemuan8.data
+
+object MataKuilah {
+    val options = listOf (
+        "pemrograman aplikasi mobile lanjut",
+        "pengembangan aplikasi web framework",
+        "prototyping",
+        "multimedia"
+    )
+}
---
+++ app/src/main/java/com/example/pertemuan8/model/RencanaStudy.kt
@@ -0,0 +1,9 @@
+package com.example.pertemuan8.model
+
+import com.example.pertemuan8.data.MataKuilah
+import com.example.pertemuan8.data.RuangKelas
+
+class RencanaStudy {
+    val namaMk: String = "",
+    val kelas: String = "",
+}
