c87ba6e3e6826ced40e117d1bad55e1e2548cfa3
IAMHUDA <hudamiftahul362@gmail.com>
2024-11-22 14:58:43 +0700

    menambahkan checkbox

--- app/src/main/java/com/example/pertemuan8/ui/view/screen/RencanaStudyView.kt
+++ app/src/main/java/com/example/pertemuan8/ui/view/screen/RencanaStudyView.kt
@@ -15,6 +15,7 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Notifications
+import androidx.compose.material3.Checkbox
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.RadioButton
@@ -145,9 +146,14 @@ fun RencanaStudyView(
                 Row(
                     verticalAlignment = Alignment.CenterVertically
                 ) {
-
+                    Checkbox(
+                        checked = checked,
+                        onCheckedChange = {checked = it}
+                    )
                 }
             }
         }
     }
 }
+
+
