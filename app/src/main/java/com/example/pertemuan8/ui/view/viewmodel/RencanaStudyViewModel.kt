3aaf705a4b3bc388d2f784b99002a37524ef9f92
IAMHUDA <hudamiftahul362@gmail.com>
2024-11-22 15:30:47 +0700

    menambahkan function setkelas

--- app/src/main/java/com/example/pertemuan8/ui/view/viewmodel/RencanaStudyViewModel.kt
+++ app/src/main/java/com/example/pertemuan8/ui/view/viewmodel/RencanaStudyViewModel.kt
@@ -1,16 +1,22 @@
 package com.example.pertemuan8.ui.view.viewmodel
 
 import androidx.lifecycle.ViewModel
+import androidx.room.util.copy
 import com.example.pertemuan8.model.RencanaStudy
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 
 class RencanaStudyViewModel : ViewModel{
     private val _krsState = MutableStateFlow(RencanaStudy())
     val krsStateUi: StateFlow<RencanaStudy> = _krsState.asStateFlow()
 
-    fun setMataKuliah(){
+    fun setMataKuliah(mkPilihan: String){
+        _krsState.update { stateMK -> stateMK.copy(namaMK = mkPilihan) }
+    }
 
+    fun setKelas(kelasPilihan: String){
+        _krsState.update { stateKelas -> stateKelas.copy(kelas = kelasPilihan) }
     }
 }
